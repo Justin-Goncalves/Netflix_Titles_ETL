0b7a8289b54f1fd7dd8387d025d917ab125a8291
Justin Goncalves <jvgoncalves85@gmail.com>
2024-05-15 20:28:37 -0400

    migrate model fields

--- data_converter_app/models.py
+++ data_converter_app/models.py
@@ -11,7 +11,7 @@ class StreamingContent(models.Model):
     country = models.CharField(max_length=100, blank=True)
     date_added = models.CharField(max_length=50)
     release_year = models.IntegerField()
-    rating = models.CharField(max_length=50)
+    rating = models.CharField(max_length=10)
     duration = models.CharField(max_length=50)
     listed_in = models.JSONField(default=list, blank=True)
     description = models.CharField(max_length=300)
