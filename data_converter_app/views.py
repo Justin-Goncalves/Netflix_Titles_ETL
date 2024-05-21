5601fd426d6bd5aead4ef9493c4f3b624b5c54c8
Justin Goncalves <jvgoncalves85@gmail.com>
2024-05-20 23:14:42 -0400

    fill out views.py file

--- data_converter_app/views.py
+++ data_converter_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializers import *
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+import pandas
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def StreamingContent_details(request):
+    if request.method == "GET":
+        streamingcontents = StreamingContent.objects.all()
+        serializer = StreamingContentSerializer(streamingcontents, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        if request.FILES:
+            file = pandas.read_csv(request.FILES['data']).to_dict(orient="records")
+            serializer = StreamingContentSerializer(data=file, many=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response('Successfully Uploaded')
